1bf7b07ce043f469e5a6beaea393cdab33d3901e
satwikWiz <satwik@wizcommerce.com>
2025-12-21 20:06:31 +0530

    feat : shadecn initialized

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,40 @@
-import { Route, Routes } from "react-router-dom";
-
-import IndexPage from "@/pages/index";
-import DocsPage from "@/pages/docs";
-import PricingPage from "@/pages/pricing";
-import BlogPage from "@/pages/blog";
-import AboutPage from "@/pages/about";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Button } from './components/ui/button'
+import { Input } from './components/ui/input'
+import { Textarea } from './components/ui/textarea'
+import { ModeToggle } from './components/ui/mode-toggle'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <Routes>
-      <Route element={<IndexPage />} path="/" />
-   \
-    </Routes>
-  );
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+       <Button>Click me</Button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+        <Input placeholder="Type your name" />
+        <Textarea placeholder="Type your message" />
+        <ModeToggle />
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
